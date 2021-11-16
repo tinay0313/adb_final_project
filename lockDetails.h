7637e083c5897d16a8b266cf65e573c2412e4e9f
tinay0313 <tina.yuan.1328@gmail.com>
2021-11-16 11:22:33 -0500

    added getters and setters to lockDetails.h and variable.h

--- lockDetails.h
+++ lockDetails.h
@@ -9,13 +9,45 @@
 
 #include <vector>
 #include <queue>
+#include "transaction.h"
 
 class lockDetails
 {
     /* 0 is free, 1 is read lock, 2 is write lock */
     int type;
-    vector<Transaction> owners;
-    queue<Transaction> waitingQueue;
+    vector<transaction> owners;
+    queue<transaction> waitingQueue;
+
+public:
+    /* returns lock type */
+    int getType();
+
+    /* returns owners */
+    vector<transaction> getOwners();
+
+    /* returns waiting queue */
+    queue<transaction> getWaitingQueue();
+
+    /* set lock type to free (0) */
+    void freeLock();
+
+    /* set lock type to read (1) */
+    void readLock();
+
+    /* set lock type to write (2) */
+    void writeLock();
+
+    /* add a transaction as an owner to the owners vector */
+    void addOwner(transaction t);
+
+    /* remove a transaction as an owner from the owners vector */
+    void removeOwner(transaction t);
+
+    /* add a transaction to waiting queue */
+    void addTransactionToWaitingQueue(transaction t);
+
+    /* remove a transaction from waiting queue */
+    void removeTransactionFromWaitingQueue(transaction t);
 }
 
 #endif
--- variable.h
+++ variable.h
@@ -17,6 +17,34 @@ class variable
     bool isReplicated;
     bool readable;
     lockDetails lock;
-}
+
+public:
+    /* return variable name */ 
+    string getName();
+
+    /* return variable value */
+    int getValue();
+
+    /* return isReplicated */
+    bool getIsReplicated();
+
+    /* return readable */
+    bool getReadable();
+
+    /* return lock */
+    lockDetails getLock();
+
+    /* sets isReplicated to true, default value is false */
+    void setIsReplicated();
+
+    /* sets readable to true, default value is false */
+    void setReadable();
+    
+    /* sets variable name */
+    void setName(string name);
+
+    /* sets variable value */
+    void setValue(int value);
+};
 
 #endif
